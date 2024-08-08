1b2019f2769f3fab2e900267a75a138a1bf49420
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-08-08 21:56:54 +0200

    Wip: problemas con el test que acepta escrow con ether

--- foundry/src/Towerbank.sol
+++ foundry/src/Towerbank.sol
@@ -1,7 +1,10 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.8;
-import {console} from "forge-std/Test.sol";
+import "forge-std/console.sol";
+import {console2} from "forge-std/Test.sol";
+// import {console} from "forge-std/console.sol";
+import "../lib/forge-std/src/Test.sol";
 import './IERC20.sol';
 import './Address.sol';
 import './SafeERC20.sol';
@@ -13,6 +16,7 @@ import './Ownable.sol';
  * @title Towerbank
  * @dev A smart contract for managing escrow transactions between buyers and sellers.
  */
+// contract Towerbank is ReentrancyGuard, Ownable, Test {
 contract Towerbank is ReentrancyGuard, Ownable {
     using SafeERC20 for IERC20;
     // 0.1 es 100 porque se multiplica por mil => 0.1 X 1000 = 100
@@ -41,6 +45,7 @@ contract Towerbank is ReentrancyGuard, Ownable {
     error ValueMustBeGreaterThan0();
     error SellerCantBeTheSameAsBuyer();
     error SellerApproveEscrowFirst();
+    error BuyerApproveEscrowFirst();
     error IncorretAmount();
     error EscrowIsNotFunded();
     // error EscrowHasAlreadyBeenRefund();
@@ -79,10 +84,11 @@ contract Towerbank is ReentrancyGuard, Ownable {
     struct Escrow {
         address payable buyer; //Comprador
         address payable seller; //Vendedor
-        uint256 value; //Monto compra
+        uint256 value; //Valor en venta en moneda 1
+        uint256 cost; //Monto compra en moneda 2
         uint256 sellerfee; //Comision vendedor
         uint256 buyerfee; //Comision comprador
-        bool escrowNative;//De Escrow, USDT o ETH
+        bool escrowNative;//De Escrow, USDT (false, por defecto) o ETH(true)
         IERC20 currency; //Moneda
         EscrowStatus status; //Estado
     }
@@ -128,117 +134,230 @@ contract Towerbank is ReentrancyGuard, Ownable {
 
     /**
      * @dev Creates a new escrow transaction.
-     * @param _seller The address of the seller.
      * @param _value The amount of the transaction.
      * @param _currency The currency token used for the transaction.
      * Requirements:
-     * - `_seller` cannot be the same as the buyer.
-     * - `_seller` cannot be the zero address.
      * - `_value` must be greater than 0.
      */
     function createEscrow(
-        address payable _seller,
+        // address payable _seller,
         uint256 _value,
+        uint256 _cost,
         IERC20 _currency
     ) external virtual {
         if(!whitelistedStablesAddresses[address(_currency)]){
             revert AddressIsNotWhitelisted();
         }
 
-        if(msg.sender == _seller){
-            revert SellerCantBeTheSameAsBuyer();
+        // if(msg.sender == _seller){
+        //     revert SellerCantBeTheSameAsBuyer();
             
-        }
-        if(_seller == address(0)){
+        // }
+        if(msg.sender == address(0)){
             revert SellerCantBeAddressZero();
-
         }
-        if(_value <= 0){
+        
+        if(_value <= 0 || _cost <= 0){
             revert ValueMustBeGreaterThan0();
         }
         uint8 _decimals = _currency.decimals();
         //Obtiene el monto a transferir desde el comprador al contrato
-        uint256 _amountFeeBuyer = ((_value * (feeBuyer * 10 ** _decimals)) /
+        uint256 _amountFeeSeller = ((_value * (feeSeller * 10 ** _decimals)) /
             (100 * 10 ** _decimals)) / 1000;
-
-        uint256 _allowance = _currency.allowance(msg.sender, address(this));
-        if(_allowance < _currency.allowance(msg.sender, address(this))){
+        if(_value + _amountFeeSeller > _currency.allowance(msg.sender, address(this))){
+        // if(_allowance < _currency.allowance(msg.sender, address(this))){
             revert SellerApproveEscrowFirst();
         }
+        //ESTA LINEA SOBRA!!
+        // uint256 _allowance = _currency.allowance(msg.sender, address(this));
+
+
+        escrows[orderId] = Escrow(
+            payable(address(0)), // Futuro comprador, buyer
+            payable(msg.sender),//creador del escrow, seller
+            _value,
+            _cost,
+            _amountFeeSeller,
+            feeBuyer,
+            false,
+            _currency,
+            EscrowStatus.Funded
+    );
 
         //Transfer USDT to contract
         _currency.safeTransferFrom(
             msg.sender,
             address(this),
-            (_value + _amountFeeBuyer)
+            (_value + _amountFeeSeller)
         );
-
-        escrows[orderId] = Escrow(
-            payable(msg.sender),
-            _seller,
-            _value,
-            feeSeller,
-            feeBuyer,
-            false,
-            _currency,
-            EscrowStatus.Funded
-        );
-
         emit EscrowDeposit(orderId, escrows[orderId]);
         orderId ++;
     }
 
     /**
     * @dev Creates a new escrow transaction with native coin.
-    * @param _seller The address of the seller.
+    * param _seller The address of the seller.
     * @param _value The amount of the transaction.
     * Requirements:
-    * - `_seller` cannot be the same as the buyer.
-    * - `_seller` cannot be the zero address.
+    // * - `_seller` cannot be the same as the buyer.
+    // * - `_seller` cannot be the zero address.
     * - `_value` must be greater than 0.
     * - The transaction value must be sufficient to cover the transaction amount plus buyer fee.
     */
     function createEscrowNativeCoin(
         // uint256 _orderId,
-        address payable _seller,
-        uint256 _value
+        // address payable _seller,
+        uint256 _value,
+        uint256 _cost
     ) external payable virtual {
 
-        if(msg.sender == _seller){
-            revert SellerCantBeTheSameAsBuyer();
+        // if(msg.sender == _seller){
+        //     revert SellerCantBeTheSameAsBuyer();
             
-        }
-        if(_seller == address(0)){
-            revert SellerCantBeAddressZero();
+        // }
+        // if(_seller == address(0)){
+        //     revert SellerCantBeAddressZero();
 
-        }
+        // }
+        
         if(_value <= 0){
             revert ValueMustBeGreaterThan0();
         }
-        
-        uint8 _decimals = 6;
+
+        // uint8 _decimals = 18;
         //Obtiene el monto a transferir desde el comprador al contrato
-        uint256 _amountFeeBuyer = ((_value * (feeBuyer * 10 ** _decimals)) /
-            (100 * 10 ** _decimals)) / 1000;
-        if(msg.value < _value + _amountFeeBuyer){
+        uint256 _amountFeeSeller = ((_value * (feeSeller * 10 ** 18)) /
+            (100 * 10 ** 18)) / 1000;
+        // require((_value + _amountFeeBuyer) <= msg.value, "Incorrect amount");
+        if(msg.value < _value + _amountFeeSeller){
             revert IncorretAmount();
         }
-        // require((_value + _amountFeeBuyer) <= msg.value, "Incorrect amount");
 
         escrows[orderId] = Escrow(
-            payable(msg.sender),
-            _seller,
+            payable(address(0)), //Futuro comprador, buyer
+            payable(msg.sender),//Creador del escrow, seller
             _value,
-            feeSeller,
+            _cost,
+            _amountFeeSeller,
             feeBuyer,
             true,
             IERC20(address(0)),
             EscrowStatus.Funded
         );
+        // (bool sent, ) = address(this).call{value: _value + _amountFeeSeller}("");
+        // require(sent, "Transfer from seller failed"); 
 
         emit EscrowDeposit(orderId, escrows[orderId]);
         orderId ++;
     }
+
+    function acceptEscrow(uint256 _orderId) external payable nonReentrant {
+        Escrow storage escrow = escrows[_orderId];
+
+//////////////////////////////////CHECKS///////////////////////////////
+
+        require(escrow.status == EscrowStatus.Funded, "Escrow is not funded");
+        require(escrow.seller != msg.sender, "Seller and buyer can't be the same");
+        // require(escrow.buyer == address(0), "Escrow already accepted");
+        // require(escrow.escrowNative, "Escrow is for native coin");
+
+//////////////////////////////////EFFETS///////////////////////////////
+        // uint256 amountFeeBuyer = (escrow.value * feeBuyer) / 10000;
+        // uint256 amountFeeSeller = (escrow.value * feeSeller) / 10000;
+        // uint256 amountFeeSeller = (escrow.value * escrow.sellerfee) / 10000;
+        uint256 amountFeeBuyer = (escrow.value * feeBuyer) / 10000;
+
+        // escrow.buyer = payable(msg.sender);
+        //NECESARIO  HACER UN APPROVE EN EL MOMENTO EN QUE EL BOB ACEPTE LA OFERTA EN EL FRONT
+        // Transfer to buyer
+        escrow.buyerfee = amountFeeBuyer;
+        escrow.buyer = payable(msg.sender);
+        console.log("Is escrowNative", escrow.escrowNative);
+
+        if (escrow.escrowNative) {
+//////////////////////////////////INTERACTIONS///////////////////////////////
+             console.log("Allowance",  escrow.currency.allowance(msg.sender, address(this)));
+             console.log("escrow.cost", escrow.cost);
+            if (escrow.cost > escrow.currency.allowance(msg.sender, address(this))){
+                revert BuyerApproveEscrowFirst();
+            }
+             console.log("Datos", msg.sender, escrow.seller, escrow.cost);
+            // Transfer tokens from buyer to seller
+            //  escrow.currency.safeTransferFrom(msg.sender, address(this), escrow.value + amountFeeBuyer);
+            // (bool sent, ) = escrow.seller.call{value: escrow.value - amountFeeSeller}("");
+            // require(sent, "Transfer to seller failed");
+            //  escrow.currency.safeTransfer(msg.sender, escrow.value - amountFeeBuyer);
+            // Transfer ETH from contract to buyer
+
+            // feesAvailableNativeCoin += amountFeeBuyer + escrow.sellerfee;
+            //  console.log("Datos tansferFrom", msg.sender, escrow.seller, escrow.cost);
+            //  escrow.currency.safeTransferFrom(msg.sender, escrow.seller, escrow.cost);
+
+            // (bool buyerSent, ) = escrow.buyer.call{value: escrow.value - amountFeeBuyer}("");
+            // require(buyerSent, "Transfer to buyer failed");
+            
+            // Refund excess value
+            // if (msg.value > escrow.value + amountFeeBuyer) {
+            //     (sent, ) = msg.sender.call{value: msg.value - (escrow.value + amountFeeBuyer)}("");
+            //     require(sent, "Refund failed");
+            // }
+            
+        } else {
+            feesAvailable[escrow.currency] += amountFeeBuyer + escrow.sellerfee;
+            // Transfer ETH from buyer to seller
+            (bool sellerSent, ) = escrow.seller.call{value: escrow.cost}("");
+            require(sellerSent, "Transfer to seller failed");            
+            require(msg.value >= escrow.cost, "Insufficient ETH value sent");
+
+            // Transfer tokens from contract to buyer
+            escrow.currency.safeTransfer(msg.sender, escrow.value - amountFeeBuyer);        
+        }
+        escrow.status = EscrowStatus.Completed;
+        // console.log("ESCROW", escrow.seller, escrow.buyer, escrow.buyerfee, escrow.sellerfee, escrow.buyerfee, escrow.value, escrow.cost, escrow.status, escrow.escrowNative);
+        emit EscrowComplete(_orderId, escrow);
+        delete escrows[_orderId];
+        // console.log("ESCROW DES", escrow.seller, escrow.buyer, escrow.buyerfee, escrow.sellerfee, escrow.buyerfee, escrow.value, escrow.cost, escrow.status, escrow.escrowNative);
+    }
+//     function acceptEscrowNativeCoin(uint256 _orderId) external payable nonReentrant {
+//         Escrow storage escrow = escrows[_orderId];
+//         require(escrow.status == EscrowStatus.Funded, "Escrow is not funded");
+//         // require(escrow.buyer == address(0), "Escrow already accepted");
+//         require(escrow.status == Funded, "Escrow already accepted");
+
+//         // uint256 amountFeeBuyer = (escrow.value * feeBuyer) / 10000;
+//         uint256 amountFeeBuyer = (escrow.value * escrow.buyerfee) / 10000;
+//         // uint256 amountFeeSeller = (escrow.value * feeSeller) / 10000;
+//         uint256 amountFeeSeller = (escrow.value * escrow.sellerfee) / 10000;
+
+//         escrow.buyer = payable(msg.sender);
+// _currency.allowance(msg.sender, address(this))
+//         if (escrow.status == 'funded') {
+//             if (escrow.value + amountFeeBuyer > escrow.currency.allowance(msg.sender, address(this))){
+//                 revert BuyerApproveEscrowFirst();
+//             };
+//             feesAvailableNativeCoin += amountFeeBuyer + amountFeeSeller;
+
+//             // Transfer to seller
+//             (bool sent, ) = escrow.seller.call{value: escrow.value - amountFeeSeller}("");
+//             require(sent, "Transfer to seller failed");
+
+//             // Refund excess value
+//             if (msg.value > escrow.value + amountFeeBuyer) {
+//                 (sent, ) = msg.sender.call{value: msg.value - (escrow.value + amountFeeBuyer)}("");
+//                 require(sent, "Refund failed");
+//             }
+//         } else {
+//             escrow.currency.transferFrom(msg.sender, address(this), escrow.value + amountFeeBuyer);
+//             feesAvailable[escrow.currency] += amountFeeBuyer + amountFeeSeller;
+
+//             // Transfer to seller
+//             escrow.currency.transfer(escrow.seller, escrow.value - amountFeeSeller);
+//         }
+
+//         escrow.status = EscrowStatus.Completed;
+
+//         emit EscrowComplete(_orderId, escrow);
+//     }
 
     /**
     * @dev Releases the escrowed funds by the contract owner.
